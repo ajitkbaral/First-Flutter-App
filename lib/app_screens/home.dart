310ce99b1012abdc264f253b30b71d6741899e83
KKEC <kkecnepal@gmail.com>
2019-10-12 11:01:11 +0545

    Image added

--- lib/app_screens/home.dart
+++ lib/app_screens/home.dart
@@ -7,80 +7,29 @@ class Home extends StatelessWidget {
       appBar: AppBar(
         title: Text("Flutter Container"),
       ),
-      body: Center(
-        child: Container(
-          padding: EdgeInsets.all(10.0),
-          alignment: Alignment.center,
-          color: Colors.purple,
-          child: Column(
-            children: <Widget>[
-              Row(
-              children: <Widget>[
-                Expanded(
-                  child: Text(
-                    "Text -1",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 35.0
-                    )
-                  )
-                ),
-                Expanded(
-                  child: Text(
-                    "Text 0",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 35.0
-                    )
-                  )
-                ),                
-              ],
-            ),
-            Row (
-              children: <Widget>[
-                Expanded(
-                  child: Text(
-                    "Text 1",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 35.0
-                    )
-                  )
-                ),
-                Expanded(
-                  child: Text(
-                    "Text 2",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 35.0
-                    )
-                  )
-                ),
-                Expanded(
-                  child: Text(
-                    "Text 3",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 35.0
-                    )
-                  )
-                ),
-                Expanded(
-                  child: Text(
-                    "Text 4",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 35.0
-                    )
-                  )
-                )
-              ],
-            )
-            ],
+      body: Column(
+        children: <Widget>[
+          FoodImageAsset(),
+          Text(
+            "This is the image of the food",
+            style: TextStyle(fontSize: 30.0),
           )
-        )
+        ],
       )
+      
     );
   }
+}
 
+class FoodImageAsset extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    Image image = Image(
+      image: AssetImage('images/image1.jpg')
+      );
+    return Container(
+      child: image,
+    );
+  }
+  
 }
